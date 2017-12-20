41090e1494a8fe2c06b65659cafeed57deb64e35
Rafaeel662 <34173000+Rafaeel662@users.noreply.github.com>
2017-12-20 11:08:56 -0200

    create kitAPI

---
+++ src/util/KitAPI.java
@@ -0,0 +1,21 @@
+package me.eventos.util;
+
+import java.util.ArrayList;
+
+import org.bukkit.entity.Player;
+
+public class KitAPI {
+	
+	public static ArrayList<Player> pvp = new ArrayList<>();
+	
+     public static void checkKit(Player p, String kit) {
+    	 
+    	 if(kit.equalsIgnoreCase("pvp")) {
+    		 
+    		 
+    		 
+    	 }
+    	 
+     }
+
+}
