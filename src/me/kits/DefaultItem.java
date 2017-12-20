6b6c886051206cd6696cfe9f6536434cb5e86bbe
Rafaeel662 <34173000+Rafaeel662@users.noreply.github.com>
2017-12-20 11:46:42 -0200

    criei uma class chamada kits

---
+++ src/me/kits/DefaultItem.java
@@ -0,0 +1,6 @@
+package me.kits;
+
+public class DefaultItem {
+	
+
+}
--- src/util/KitAPI.java
+++ src/util/KitAPI.java
@@ -12,8 +12,6 @@ public class KitAPI {
     	 
     	 if(kit.equalsIgnoreCase("pvp")) {
     		 
-    		 
-    		 
     	 }
     	 
      }
