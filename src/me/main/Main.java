67e3e55246c34733cb6eb701374bf0365e60bc25
NycollasDev <34692304+NycollasDev@users.noreply.github.com>
2017-12-20 10:44:55 -0200

    fiz a getinstance

--- src/me/main/Main.java
+++ src/me/main/Main.java
@@ -1,9 +1,23 @@
 package me.main;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	
-	public static String tag = "";
+	public static String tag = "§a§lKit§6§lPvP";
+	
+	public static Main instance;
+	public static Main getinstance() {
+		return instance;
+	}
+	
+	public void onEnable() {
+		Bukkit.getServer().getConsoleSender().sendMessage("§a§lPlugin Iniciado com Sucesso By: Rafael662 & NycollasDev");
+	}
+	
+	public void onDisable() {
+		Bukkit.getServer().getConsoleSender().sendMessage("§a§lPlugin Desativado com Sucesso By: Rafael662 & NycollasDev");
+	}
 
 }
